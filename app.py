68e409a34137d9f04f0f49ec2332778940e9d98d
hieudtbv-afk <hieudtbv@gmail.com>
2026-01-21 16:20:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ st.set_page_config(
 # =====================
 # 2. THÔNG TIN GOOGLE SHEETS
 # =====================
-SHEET_ID = "ukN4ftXcAtRidpv26"
+SHEET_ID = "1vHPkRbZGxhLZr9N60tFyKzgUkbnRKB_-Dg7FaCiqtBo"
 SHEET_NAME = "Form_Responses"
 
 csv_url = (
